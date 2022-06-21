619ccee9c8ef6d0abf6feb026183163d8897ae9a
nikocarba <n.carballal@gmail.com>
2022-06-21 10:34:08 -0300

    Agrego comentarios

--- Ejercicios tecnicos/Ejercicio_Técnico2.py
+++ Ejercicios tecnicos/Ejercicio_Técnico2.py
@@ -7,11 +7,11 @@ os.chdir('Ejercicios tecnicos')
 df = pd.read_excel('datasets/dataset_ejercicio1.xlsx')
 
 #Transform
-df = df[df['shipping_cost'] >0]
-df['dia_mes_anio'] = df['date'].dt.strftime('%d-%m-%Y')
-df['org_dest'] = df['origin_province'] + '-' + df['destination_province']
-df = df.groupby(['dia_mes_anio','org_dest']).agg({'shipping_cost':['min','mean','max']}).reset_index()
-df.columns = ['dia_mes_anio', 'org_dest', 'min_shipping_cost', 'avg_shipping_cost', 'max_shipping_cost']
+df = df[df['shipping_cost'] >0]  #filtro de aquellos registros con shipping_cost menor o igual a 0
+df['dia_mes_anio'] = df['date'].dt.strftime('%d-%m-%Y') 
+df['org_dest'] = df['origin_province'] + '-' + df['destination_province'] #concatenacion y generacion de nuevo campo
+df = df.groupby(['dia_mes_anio','org_dest']).agg({'shipping_cost':['min','mean','max']}).reset_index() #agrupamiento y calculo de minimo promedio y maximo
+df.columns = ['dia_mes_anio', 'org_dest', 'min_shipping_cost', 'avg_shipping_cost', 'max_shipping_cost'] #renombre de columnas
 
 #Load
-df.to_json('output/envios.json', orient='records', force_ascii=False)
+df.to_json('output/envios.json', orient='records', force_ascii=False) #archivo final guardado en la carpeta output
