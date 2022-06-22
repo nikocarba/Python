5622e01085917cd396b8332885f6bb30ba8c5162
nikocarba <n.carballal@gmail.com>
2022-06-22 11:34:10 -0300

    se agregan logs

--- Ejercicios tecnicos/Ejercicio_tecnico3.py
+++ Ejercicios tecnicos/Ejercicio_tecnico3.py
@@ -2,20 +2,25 @@ import pandas as pd
 import datetime as dt
 import pyodbc
 
+time = dt.datetime.now().strftime('%Y-%m-%d')
+filename = 'carga_{}.log'.format(time)
+f = open(filename, 'w') #Archivo de logs
 
 #Extract
 url = 'https://adlschallenge.blob.core.windows.net/challenge/nuevas_filas.csv?sp=r&st=2022-06-20T14:51:53Z&se=2022-12-31T22:51:53Z&spr=https&sv=2021-06-08&sr=b&sig=y9hLJFCVvGh1Ej58SXqsXTSVC6ABoVuQgfECDOd83Lw%3D'
 try:
     df = pd.read_csv(url)
 except:
-    print('No se pudo descargar el archivo.')
+    f.write('{} - No se pudo descargar el archivo.\n'.format(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
 if df.shape[0] == 0:
-    print('Archivo vacio.')
+    f.write('{} - Archivo vacio.\n'.format(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
     exit()
 
 #Transform
+df.dropna(subset=['ID', 'MUESTRA', 'RESULTADO'], inplace=True) #dropea filas con keys nulos
 df.drop_duplicates(['ID', 'MUESTRA', 'RESULTADO'], keep='last', inplace=True) #dropea los duplicados
-df['FECHA_COPIA'] = dt.datetime.now() #pobla el campo FECHA_COPIA con la fecha y hora de carga
+df['FECHA_COPIA'] = time #pobla el campo FECHA_COPIA con la fecha y hora de carga
+registros = len(df)
 
 
 #Load
@@ -30,11 +35,14 @@ cursor = conn.cursor()
 try:
     cursor.executemany(f"INSERT INTO prueba VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", df.values.tolist())
     conn.commit()
-    print(f'{len(df)} rows inserted to the prueba table.')
+    f.write('{} - {} registros cargados.\n'.format(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'), registros))
 except:
-    print('Falla en el proceso no se cargó ningun registro.')
+    f.write('{} - Falla en el proceso no se cargó ningun registro.\n'.format(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
 
-cursor.execute('SELECT count(*) FROM prueba')
-cursor.fetchone()[0]
+#Verificar que no hubo perdida de informacion
+cursor.execute('SELECT count(*) FROM prueba where fecha_copia = (select max(fecha_copia) from prueba)') #asumiendo que nunca se modifique la fecha del sistema
 
+f.write('{} - Se perdieron {} registros.\n'.format(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'), registros - cursor.fetchone()[0]))
+
+f.close()
 cursor.close()
